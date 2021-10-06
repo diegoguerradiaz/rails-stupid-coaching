95fe201d7403dc3450b0b6d876d60bc97ade706f
Diego Guerra <diegoguerradiaz@gmail.com>
2021-10-05 22:11:12 -0300

    corrected if else code to answer to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,20 +2,15 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
-    @answer = respond
-  end
-
-  private
-
-  def respond(answer)
-    if answer == 'I am going to work'
+    if @question == 'I am going to work'
       then 'Great!'
-    elsif answer.include? '?'
+    elsif @question.include? '?'
       then 'Silly question, get dressed and go to work!'
     else
       'I dont care, get dressed and go to work!'
     end
   end
+
 
   # If the message is I am going to work, the coach will answer Great!
   # If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
