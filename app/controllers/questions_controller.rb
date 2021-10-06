cc6faa1ced1f0158e98c3057357ebd8ec3327c52
Diego Guerra <diegoguerradiaz@gmail.com>
2021-10-05 21:49:31 -0300

    added the if else clause for the answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,23 @@
 class QuestionsController < ApplicationController
-  def ask
+  def ask; end
+
+  def answer
+    @answer = respond
   end
+
+  private
+
+  def respond(answer)
+    if answer == 'I am going to work'
+      then 'Great!'
+    elsif answer.include? '?'
+      then 'Silly question, get dressed and go to work!'
+    else
+      'I dont care, get dressed and go to work!'
+    end
+  end
+
+  # If the message is I am going to work, the coach will answer Great!
+  # If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+  # Otherwise the coach will answer I don't care, get dressed and go to work!
 end
