95459afc3b961c5102f63c16c4eba6c79812aeac
Diego Guerra <diegoguerradiaz@gmail.com>
2021-10-07 21:44:59 -0300

    created elsif code

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,17 +2,13 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
+    @question = params[:answer]
     if @question == 'I am going to work'
-      then 'Great!'
-    elsif @question.include? '?'
-      then 'Silly question, get dressed and go to work!'
+      @response = 'Great!'
+    elsif @question.end_with?('?')
+      @response = 'Silly question, get dressed and go to work!'
     else
-      'I dont care, get dressed and go to work!'
+      @response = 'I dont care, get dressed and go to work!'
     end
   end
-
-
-  # If the message is I am going to work, the coach will answer Great!
-  # If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
-  # Otherwise the coach will answer I don't care, get dressed and go to work!
 end
